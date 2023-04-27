58392f6606823f81f5e4bb025e3bad3b470e36c7
Janne Mattila <janne.mattila@gofore.com>
2023-04-27 17:10:39 +0300

    convert to TS

--- reactapp/src/App.tsx
+++ reactapp/src/App.tsx
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import CodeInputPage from '@my-dummy-namespace/dummy-component';
 
@@ -7,10 +6,6 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
