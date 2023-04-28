a82c6de0eba97a94f15e15b63c6e386349748fcb
Janne Mattila <janne.mattila@gofore.com>
2023-04-28 09:20:01 +0300

    simplify build-buildpacks.sh

--- reactapp/build-buildpacks.sh
+++ reactapp/build-buildpacks.sh
@@ -9,15 +9,6 @@ echo Building OCI container image using buildpacks. Building in $BASEDIR
 pack build test/frontend-nginx \
  --clear-cache \
  --builder paketobuildpacks/builder:base \
- --buildpack paketobuildpacks/ca-certificates:3.6.1 \
- --buildpack paketobuildpacks/node-engine:1.4.1 \
- --buildpack paketobuildpacks/npm-install:1.1.0 \
- --buildpack paketobuildpacks/node-run-script:1.0.4 \
- --buildpack paketobuildpacks/nginx:0.13.2 \
- --buildpack paketobuildpacks/source-removal:0.2.0 \
- --env BP_NODE_RUN_SCRIPTS=build \
  --env BP_WEB_SERVER=nginx \
  --env BP_DISABLE_SBOM=true \
- --env BP_WEB_SERVER_ROOT=build \
- --env BP_NODE_VERSION="18.14.2"
-
+ --env BP_NODE_VERSION="18.16"
