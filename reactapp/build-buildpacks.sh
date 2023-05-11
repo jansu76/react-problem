dc9d0036c6e534cea7704464fd4b2c8e809b881b
Janne Mattila <janne.mattila@gofore.com>
2023-05-11 11:57:23 +0300

    add build-after-pause for debugging

--- reactapp/build-buildpacks.sh
+++ reactapp/build-buildpacks.sh
@@ -9,7 +9,7 @@ echo Building OCI container image using buildpacks. Building in $BASEDIR
 pack build myapp/frontend-nginx \
  --clear-cache \
  --builder paketobuildpacks/builder:base \
- --env BP_NODE_RUN_SCRIPTS=build \
+ --env BP_NODE_RUN_SCRIPTS=build-after-pause \
  --env BP_WEB_SERVER=nginx \
  --env BP_DISABLE_SBOM=true \
  --env BP_WEB_SERVER_ROOT=build
